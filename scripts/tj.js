98a12a5428f40b3181db5d97b3fa56ba46f76ff6
12123ads <3127086580@qq.com>
2022-07-15 00:10:34 +0800

    tj

---
+++ scripts/tj.js
@@ -0,0 +1,3 @@
+hexo.extend.filter.register('theme_inject', function(injects) {
+    injects.footer.raw('default', '<script async defer data-website-id="d78ef877-fd6c-49be-889c-aa098c7ddbaf" src="https://tj.xzh.gs/umami.js"></script>');
+  });
