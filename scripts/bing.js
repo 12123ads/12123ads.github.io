35f79c5419d5a81fa4516d7030c81195de3b8eae
12123ads <3127086580@qq.com>
2022-07-18 14:52:32 +0800

    bing

--- scripts/bing.js
+++ scripts/bing.js
@@ -1,3 +1,3 @@
 hexo.extend.filter.register('theme_inject', function(injects) {
-    injects.footer.raw('default', '<meta name="msvalidate.01" content="61145986C3E46627A3BF1063D321A136" />');
+    injects.head.raw('default', '<meta name="msvalidate.01" content="61145986C3E46627A3BF1063D321A136" />');
   });
