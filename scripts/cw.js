bd14b84df089b0dde91f37aa3f6986dacc909086
12123ads <3127086580@qq.com>
2022-09-03 10:51:11 +0800

    cw

--- scripts/cw.js
+++ scripts/cw.js
@@ -1,3 +1,3 @@
-hexo.extend.filter.register('theme_inject', function(injects) {
-    injects.head.file('cw', 'source/js/cw.js', { key: 'value' }, -1);
-  });
+// hexo.extend.filter.register('theme_inject', function(injects) {
+//     injects.head.file('cw', 'source/js/cw.js', { key: 'value' }, -1);
+//   });
